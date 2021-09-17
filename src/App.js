412dfb13cd8f41652dc9cfbc7d387174e968d89f
pratik8696 <mepratikanand@gmail.com>
2021-09-17 19:02:19 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import HomeScreen from './components/HomeScreen/HomeScreen';
+import Login from './components/Login/Login';
+import { Link, BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <>
-    <HomeScreen />
+      <Router>
+        <Switch>
+
+          <Route path="/" exact>
+            <HomeScreen />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+        </Switch>
+      </Router>
     </>
   );
 }
